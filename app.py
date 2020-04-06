818f79cdfc1444027f9acddb7753b7cfbce6677f
jeanmarcdav <jpd26362@email.vccs.edu>
2020-04-06 00:10:22 -0400

    created helper for calculating distances

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
 import plotly.express as px
 import pandas as pd
 import csv
+from latlongHelper import addressToLatLong, batchAdressConverter, calculateDistance, batchDistanceCalculation
+
 # TODO: utilize public tax info to generate this, use zillowHelper.py to estimate/display realestate/rent costs
+# set these before using
+mapQuestKey = "" # get key from mapquest https://developer.mapquest.com/
+targetAddress = "" # <street number> <name> <type> e.g. 1234 anywhere blvd
+targetCityStateZip = "" # <city> <state abbreviation> <zip> e.g. Podunk VA 20170
+
 entries = {
     'address': [],
     'citystatezip': [],
     'price': [],
     'distance': [],
 }
+# wiehle reston metro is default location
+if targetAddress == "" or targetCityStateZip == "":
+    targetAddress = "1908 Reston Station Blvd"
+    targetCityStateZip = "Reston VA 20190"
+convertedTarget = addressToLatLong(targetAddress, targetCityStateZip, mapQuestKey)
 with open('addresses.csv', mode='r') as infile:
     reader = csv.reader(infile)
     for row in reader:
         entries['address'].append(row[0])
         entries['citystatezip'].append(row[1])
         entries['price'].append(row[2])
-        entries['distance'].append(row[3])
 
 n_entries = len(entries['address'])
+locationsLatLong = batchAdressConverter(entries['address'], entries['citystatezip'], mapQuestKey)
+entries['distance'] = batchDistanceCalculation(locationsLatLong, convertedTarget)
 
 df = pd.DataFrame(dict(distance=entries['distance'], price=entries['price'],
                        citystatezip=entries['citystatezip']))
@@ -25,5 +38,11 @@ fig = px.scatter(df, x="distance", y="price",
                  title="Distance From Metro vs. Price",
                  hover_name="citystatezip", hover_data=['citystatezip'],
                 )
+fig.update_layout(
+    xaxis=dict(
+        range=(0, 2),
+        constrain='domain'
+    )
+)
 
 fig.show()
---
+++ latlongHelper.py
@@ -0,0 +1,51 @@
+import requests, math
+
+def addressToLatLong(address, citystatezip, key):
+    parameters = {
+        'location': address + ' ' + citystatezip,
+        'key': key,
+    }
+    searchEndpoint = "http://open.mapquestapi.com/geocoding/v1/address"
+    headers = {'Accept': 'application/json'}
+    r = requests.get(url = searchEndpoint, headers=headers, params = parameters)
+    data = r.json()["results"][0]["locations"][0]["displayLatLng"]
+    return (data["lat"],data["lng"])
+
+def batchAdressConverter(addresses, citystatezips, key):
+    output = []
+    locationsList = []
+    for i in range(len(addresses)):
+        locationsList.append([addresses[i] + citystatezips[i]])
+    parameters = {
+        'location': locationsList,
+        'key': key,
+    }
+    searchEndpoint = "http://www.mapquestapi.com/geocoding/v1/batch"
+    headers = {'Accept': 'application/json'}
+    r = requests.get(url=searchEndpoint, headers=headers, params=parameters)
+    for i in range(len(r.json()["results"])):
+        data = r.json()["results"][i]["locations"][0]["displayLatLng"]
+        output.append((data["lat"], data["lng"]))
+    return output
+
+def calculateDistance(loc1, loc2):
+    # initial variables and calculations
+    R = 6373.0 # radius of Earth
+    lat1 = math.radians(loc1[0])
+    lon1 = math.radians(loc1[1])
+    lat2 = math.radians(loc2[0])
+    lon2 = math.radians(loc2[1])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    # Haversine formula
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
+
+def batchDistanceCalculation(locations, target):
+    output = []
+    for location in locations:
+        output.append(calculateDistance(location, target))
+    return output
