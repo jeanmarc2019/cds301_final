3b8b43c7e9c29792015231619e9f6cfff222acc2
jeanmarcdav <jpd26362@email.vccs.edu>
2020-05-07 09:32:57 -0400

    added mapbox integration

--- app.py
+++ app.py
@@ -8,7 +8,23 @@ mapQuestKey = "" # REQUIRED: get key from mapquest https://developer.mapquest.co
 targetAddress = "" # (optional) <street number> <name> <type> e.g. 1234 anywhere blvd
 targetCityStateZip = "" # (optional) <city> <state abbreviation> <zip> e.g. Podunk VA 20170
 appraisalType = "APRTOT" # appraisal options are APRTOT, APRBLDG, and APRLAND
-sampleSize = 500 # anything higher than 10 takes a while TODO: optimize code to decrease time
+sampleSize = 3 # anything higher than 10 takes a while TODO: optimize code to decrease time
+numOfBins = 8
+colors = []
+
+def create_bins(lower_bound, width, quantity):
+    bins = []
+    for low in range(lower_bound,
+                     lower_bound + quantity * width + 1, width):
+        bins.append((low, low + width))
+    return bins
+
+
+def find_bin(value, bins):
+    for i in range(0, len(bins)):
+        if bins[i][0] <= value < bins[i][1]:
+            return i
+    return -1
 
 # wiehle reston metro is default location
 if targetAddress == "" or targetCityStateZip == "":
@@ -28,11 +44,31 @@ print(len(entries['address']))
 print(len(entries['citystatezip']))
 print(len(entries['distance']))
 print(len(entries['price']))
+lats = []
+longs = []
+for location in locationsLatLong:
+    lats.append(location[0])
+    longs.append(location[1])
+
+# meanPrice = 0
+# sigma = 0
+# for price in entries['price']:
+#     meanPrice += price
+# meanPrice /= len(entries['price'])
+# for price in entries['price']:
+#     sigma += (price - meanPrice) ** 2
+# sigma /= len(entries['price'])
+# sigma **= 0.5
+# bins = create_bins(lower_bound=min(entries['price']),
+#                    width=sigma,
+#                    quantity=numOfBins)
+# entries['bin'] = [find_bin(entries['price'][i], bins) for i in range(len(entries['price']))]
+
 combinedNames = [entries['address'][i] + ', ' + entries['citystatezip'][i] for i in range(len(entries['address']))]
-df = pd.DataFrame(dict(distance=entries['distance'], price=entries['price'],
-                       name=combinedNames))
 
 name = targetAddress if targetAddress != "1908 Reston Station Blvd" else "Wiehle-Reston East Metro"
+df = pd.DataFrame(dict(distance=entries['distance'], price=entries['price'],
+                       name=combinedNames, lats=lats, longs=longs))
 # Use column names of df for the different parameters x, y, color, ...
 fig = px.scatter(df, x="distance", y="price",
                  title="Distance From "+name+" vs. Price",
@@ -45,4 +81,18 @@ fig.update_layout(
     )
 )
 print("DONE")
+fig.show()
+# add target address
+lats.append(convertedTarget[0])
+longs.append(convertedTarget[1])
+combinedNames.append(targetAddress)
+entries['distance'].append(0)
+entries['price'].append(0)
+df = pd.DataFrame(dict(distance=entries['distance'], price=entries['price'],
+                       name=combinedNames, lats=lats, longs=longs))
+fig = px.scatter_mapbox(df, lat="lats", lon="longs", hover_name="name", hover_data=['price'],
+                        color_discrete_sequence=["fuchsia"], zoom=15, height=600,
+                        center=dict(lat=convertedTarget[0], lon=convertedTarget[1]))
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
